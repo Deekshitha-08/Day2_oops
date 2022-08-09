813c410c34a882d67d3d6ef47c2d7ae5244d35fc
Sivakoti Sai Deekshitha <Sivakoti.Deekshitha@ivycomptech.com>
2022-08-09 17:04:22 +0530

    Holiday

--- Day2_oops/src/Holiday.java
+++ Day2_oops/src/Holiday.java
@@ -2,18 +2,22 @@ public class Holiday {
     public static void main(String[] args) {
 
     }
+
     private String name;
     private int day;
     private String month;
-    public Holiday(String n, int d, String m){
+
+    public Holiday(String n, int d, String m) {
         name = n;
         day = d;
         month = m;
     }
-    public boolean inSameMonth (Holiday hol){
+
+    public boolean inSameMonth(Holiday hol) {
         return this.month.equals(hol.month);
     }
-    public static double avgDate (Holiday[]hol){
+
+    public static double avgDate(Holiday[] hol) {
         int sum = 0;
         for (int i = 0; i < hol.length; i++) {
             sum = sum + hol[i].day;
@@ -21,9 +25,14 @@ public class Holiday {
         return ((double) sum) / hol.length;
     }
 
-
-
+    Holiday in = new Holiday("Independence Day", 4, "July");
 }
+
+
+
+
+
+
 
 
 
